22e4ea7ac0af9d9fc4809498e6107e185f454fd2
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-04-06 19:12:51 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 curl -L https://github.com/crunchy-labs/crunchy-cli/releases/download/v2.3.6/crunchy-v2.3.6_linux \
-    -o /usr/local/bin/crunchy-cli && chmod +x /usr/local/bin/crunchy-cli
+    -o /usr/local/bin/crunchy && chmod +x /usr/local/bin/crunchy
+crunchy --etp-rt $COOKIE
+crunchy archive https://www.crunchyroll.com/series/GY8VEQ95Y/darling-in-the-franxx
