6452cb9cdb3afd500206d9fc7290d4ddb526cbf0
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-04-06 19:05:03 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
-git clone https://github.com/crunchy-labs/crunchy-cli
-cd crunchy-cli
-cargo build --release
-cargo install --force --path .
+curl -L https://github.com/crunchy-labs/crunchy-cli/releases/download/v2.3.6/crunchy-v2.3.6_linux \
+    -o /usr/local/bin/crunchy-cli && chmod +x /usr/local/bin/crunchy-cli
