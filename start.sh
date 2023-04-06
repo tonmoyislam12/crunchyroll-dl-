170c3e6171799d2ea364859a397595d775c89a4b
Tonmoy <127471199+tonmoyislam12@users.noreply.github.com>
2023-04-06 19:22:00 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 curl -L https://github.com/crunchy-labs/crunchy-cli/releases/download/v2.3.6/crunchy-v2.3.6_linux \
     -o /usr/local/bin/crunchy && chmod +x /usr/local/bin/crunchy
 crunchy --etp-rt $COOKIE
-crunchy archive https://www.crunchyroll.com/series/GY8VEQ95Y/darling-in-the-franxx
+crunchy archive https://www.crunchyroll.com/series/GNVHKN7M4/shikimoris-not-just-a-cutie[S1E4-S1E12]
